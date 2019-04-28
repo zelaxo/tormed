b430750ccbe16adb5366378bb39b52fb84337cb9
zaygo <zaygo@mail.com>
2019-04-28 11:38:08 +0530

    Basic Model

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.urlencoded({extended:true}));
 const port = process.env.PORT || 3000;
 
 aws.config.update({
-    accessKeyId : 'AKIAJLI4URTABVKBM7TQ',
-    secretAccessKey : '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'
+    accessKeyId : process.env.AKI,
+    secretAccessKey : process.env.SAK
 });
 
-const s3 = aws.S3;
+const s3 = new aws.S3();
 
 let params = {
     Bucket : 'tormed',
@@ -22,19 +22,26 @@ let params = {
     Body : null
 }
 
-app.post('/', (req,res) => {
+app.post('/murl', (req,res) => {
     let magnet = req.body.mag;
     console.log(`New MagnetUrl received : ${magnet}`);
     let client = new WebTorrent();
-    client.add(magnet, (torrent) => {
+    client.add(magnet, {path:`${__dirname}/downloads`}, (torrent) => {
         console.log(`Downloading : ${torrent.infoHash}`);
-        torrent.files.forEach((file) => {
-            params.Key = file;
-            params.Body = fs.readFileSync(file);
-            s3.putObject(params,(err) => {
-                if (err) throw err;
-                console.log("Torrent successfully downloaded!");
-            }); 
+        torrent.on('done', () => {
+            console.log('Torrent successfully downloaded to server!');
+            torrent.files.forEach((file) => {
+                params.Key = file.name;
+                params.Body = fs.readFileSync(`${__dirname}/downloads/${file.name}`);
+                s3.putObject(params,(err) => {
+                    if (err) throw err;
+                    console.log(`${file.name} transfered to storage!`);
+                    fs.unlink(`${__dirname}/downloads/${file.name}`, (err) => {
+                        if (err) throw err;
+                        return console.log(`${file.name} cleared from server`);
+                    });
+                }); 
+            });
         });
     });
 });
